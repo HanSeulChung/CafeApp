379942e832f83982fb393185fa829f752e9b1b90
HanSeul <w0w1278@naver.com>
2023-10-18 11:34:15 +0900

    init

---
+++ src/main/java/com/example/cafeapp/CafeAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.cafeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cafeapp/CafeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cafeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
