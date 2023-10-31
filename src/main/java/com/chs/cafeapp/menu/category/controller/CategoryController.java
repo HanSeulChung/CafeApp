99dbd562f9500d5b39ab6b61b8a44b5d90b68f0e
HanSeul <w0w1278@naver.com>
2023-10-31 09:36:42 +0900

    refactor: Category Controller 메서드 주석 @param, @return, @thorw 작성

--- src/main/java/com/chs/cafeapp/menu/category/controller/CategoryController.java
+++ src/main/java/com/chs/cafeapp/menu/category/controller/CategoryController.java
@@ -5,6 +5,7 @@ import com.chs.cafeapp.menu.category.dto.CategoryEditInput;
 import com.chs.cafeapp.menu.category.dto.CategoryInput;
 import com.chs.cafeapp.menu.category.dto.CategoryResponse;
 import com.chs.cafeapp.menu.category.service.CategoryService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import com.chs.cafeapp.exception.CustomException;
 
 /**
  * 카테고리 CRUD Controller
@@ -28,16 +30,23 @@ public class CategoryController {
 
   /**
    * 카테고리 생성 Controller
+   * @param categoryInput 등록할 category id와 변경할 대분류, 중분류 카테고리 이름
+   * @return CategoryResponse: 등록한 대분류, 중분류 category 반환
+   * @throws CustomException: 등록할 대분류, 중분류 category가 이미 존재할 때 CustomException 발생
    */
   @PostMapping()
-  public CategoryResponse addCategory(@RequestBody CategoryInput request) {
-    CategoryDto categoryDto = categoryService.add(request);
+  public CategoryResponse addCategory(@RequestBody CategoryInput categoryInput) {
+    CategoryDto categoryDto = categoryService.add(categoryInput);
     var result = CategoryResponse.toResponse(categoryDto);
     return result;
   }
 
   /**
    * 카테고리 수정 Controller
+   * @param categoryEditInput: 수정할 category id와 변경할 대분류, 중분류 카테고리 이름
+   * @return CategoryResponse: 수정한 대분류, 중분류 category 반환
+   * @throws CustomException: 수정한 대분류, 중분류 category가 이미 존재할 때,
+   *                          category id가 없을 경우 CustomException 발생
    */
   @PutMapping()
   public CategoryResponse editCategory(@RequestBody CategoryEditInput categoryEditInput) {
@@ -48,27 +57,33 @@ public class CategoryController {
 
   /**
    * 카테고리 삭제 Controller
+   * @param categoryId: 삭제할 카테고리 id
+   * @return String "해당 카테고리를 삭제했습니다."
+   * @throws CustomException: 카테고리 id가 존재하지 않는 경우 CustomException 발생
    */
   @DeleteMapping()
-  public ResponseEntity<?> deleteCategory(@RequestParam Long categoryId) {
+  public ResponseEntity<String> deleteCategory(@RequestParam Long categoryId) {
     categoryService.delete(categoryId);
     return ResponseEntity.ok("해당 카테고리를 삭제했습니다.");
   }
 
   /**
-   * 카테고리 전체 조회
+   * 카테고리 전체 조회 Controller
+   * @return List<CategoryResponse>: 등록되어있는 카테고리 전체 반환됨
    */
   @GetMapping()
-  public ResponseEntity<?> readAllCategory() {
+  public ResponseEntity<List<CategoryResponse>> readAllCategory() {
     var result = CategoryResponse.toResponse(categoryService.readAll());
     return ResponseEntity.ok(result);
   }
 
   /**
-   * 카테고리 대분류로 조회
+   * 카테고리 대분류로 조회 Controller
+   * @param superCategory: 조회할 대분류 카테고리 이름
+   * @return List<CategoryResponse>: 해당 대분류 카테고리 이름으로 등록되어있는 카테고리들이 List로 반환됨
    */
   @GetMapping("{superCategory}")
-  public ResponseEntity<?> readSuperCategory(@PathVariable("superCategory") String superCategory) {
+  public ResponseEntity<List<CategoryResponse>> readSuperCategory(@PathVariable("superCategory") String superCategory) {
     var result = CategoryResponse.toResponse(categoryService.readSuperCategory(superCategory));
     return ResponseEntity.ok(result);
   }
