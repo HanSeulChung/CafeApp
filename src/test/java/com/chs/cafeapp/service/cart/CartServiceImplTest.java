7f9114d96c947437669414bdf9933742852f4d40
HanSeul <w0w1278@naver.com>
2023-10-31 22:43:28 +0900

    refactor: 안쓰는 주석 없애기

--- src/test/java/com/chs/cafeapp/service/cart/CartServiceImplTest.java
+++ src/test/java/com/chs/cafeapp/service/cart/CartServiceImplTest.java
@@ -2,7 +2,6 @@ package com.chs.cafeapp.service.cart;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
