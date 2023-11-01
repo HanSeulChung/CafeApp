d03028577a8243979881dc986b804eb62096048a
HanSeul <w0w1278@naver.com>
2023-11-01 16:22:25 +0900

    docs: OrderAdminController 주석 처리

--- src/main/java/com/chs/cafeapp/order/controller/OrderAdminController.java
+++ src/main/java/com/chs/cafeapp/order/controller/OrderAdminController.java
@@ -19,13 +19,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderAdminController {
   private final OrderService orderService;
 
+  /**
+   * 전체 주문 조회 Controller
+   * @return List<OrderDto>: 전체 주문 List로 반환
+   */
   @GetMapping()
   public ResponseEntity<List<OrderDto>> viewAllOrders() {
     return ResponseEntity.ok(orderService.viewAllOrders());
   }
 
+  /**
+   * 주문 상태별로 주문 조회 Controller
+   * @param orderStatusNum: OrderStatus의 num값
+   * @return List<OrderDto>: 해당 orderStatusNum에 해당되는 주문들 List로 반환
+   */
   @GetMapping("/order-status/{orderStatus}")
-  public ResponseEntity<List<OrderDto>> viewOrdersByOrderStatus(@PathVariable int orderStatus) {
-    return ResponseEntity.ok(orderService.viewOrdersByOrderStatus(orderStatus));
+  public ResponseEntity<List<OrderDto>> viewOrdersByOrderStatus(@PathVariable int orderStatusNum) {
+    return ResponseEntity.ok(orderService.viewOrdersByOrderStatus(orderStatusNum));
   }
 }
