0fd3064c8ef213920006df2d4425267c4f474e06
HanSeul <w0w1278@naver.com>
2023-11-01 11:08:44 +0900

    fix about test: DataJpaTest시 cart.getCartMenu() 가 null인 현상 fix
    em.clear()로 캐시 삭제, 영속성컨텍스트에 대해서 더 정확히 알아야하는 이유

---
+++ src/main/java/com/chs/cafeapp/order/controller/OrderAdminController.java
@@ -0,0 +1,13 @@
+package com.chs.cafeapp.order.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Admin 주문 Controller
+ */
+@RestController
+@RequestMapping("/admin/orders")
+public class OrderAdminController {
+
+}
