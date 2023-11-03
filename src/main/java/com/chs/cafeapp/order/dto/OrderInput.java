c76eaf6f627535e062a120ded0438e5f35bf4092
HanSeul <w0w1278@naver.com>
2023-11-03 22:27:03 +0900

    refactor: 주문시 쿠폰 사용 유무에 따른 couponId 포함

--- src/main/java/com/chs/cafeapp/order/dto/OrderInput.java
+++ src/main/java/com/chs/cafeapp/order/dto/OrderInput.java
@@ -1,17 +1,36 @@
 package com.chs.cafeapp.order.dto;
 
-import lombok.AllArgsConstructor;
+import javax.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
-@Builder
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class OrderInput {
+  @NotNull
   private long menuId;
+  @NotNull
   private String menuName;
+  @NotNull
   private int menuPrice;
+  @NotNull
   private int quantity;
+  @NotNull
   private boolean couponUse;
 
+  private Long couponId;
+
+  @Builder
+  public OrderInput(long menuId, String menuName, int menuPrice, int quantity, boolean couponUse) {
+    this.menuId = menuId;
+    this.menuName = menuName;
+    this.menuPrice = menuPrice;
+    this.quantity = quantity;
+    this.couponUse = couponUse;
+  }
+
+  public void setCouponId(Long couponId) {
+    this.couponId = couponId;
+  }
 }
