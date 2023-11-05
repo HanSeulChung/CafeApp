e163a9b6fee10d6ce4626388ea13eb6d284cbc2a
HanSeul <w0w1278@naver.com>
2023-11-05 20:36:00 +0900

    style: Controller 주석 처리

--- src/main/java/com/chs/cafeapp/coupon/controller/CouponController.java
+++ src/main/java/com/chs/cafeapp/coupon/controller/CouponController.java
@@ -20,16 +20,34 @@ import org.springframework.web.bind.annotation.RestController;
 public class CouponController {
   private final CouponService couponService;
 
+  /**
+   * 쿠폰 전체 조회 Controller
+   * @param userId: 쿠폰 조회할 사용자의 id
+   * @return List<CouponResponse>: 사용자가 가지고 있는 모든 쿠폰 List 반환
+   *                                기존 CouponDto에서 사용 가능, 사용 완료, 기간 만료 등의 statusName 추가
+   */
   @GetMapping()
   public ResponseEntity<List<CouponResponse>> viewAllCoupons(@RequestParam String userId) {
     return ResponseEntity.ok(couponService.viewAllCoupons(userId));
   }
 
+  /**
+   * 사용 가능한 쿠폰 전체 조회 Controller
+   * @param userId: 쿠폰 조회할 사용자의 id
+   * @return List<CouponResponse>: 사용자가 가지고 있는 모든 쿠폰 중 사용 가능한 쿠폰 List 반환
+   *                                기존 CouponDto에서 사용 가능, 사용 완료, 기간 만료 등의 statusName 추가
+   */
   @GetMapping("/usable")
   public ResponseEntity<List<CouponResponse>> viewAllCanUseCoupons(@RequestParam String userId) {
     return ResponseEntity.ok(couponService.viewAllCanUseCoupons(userId));
   }
 
+  /**
+   * 사용 불가능한 쿠폰 전체 조회 Controller
+   * @param userId: 쿠폰 조회할 사용자의 id
+   * @return List<CouponResponse>: 사용자가 가지고 있는 모든 쿠폰 중 사용 불가능한 쿠폰 List 반환
+   *                                기존 CouponDto에서 사용 가능, 사용 완료, 기간 만료 등의 statusName 추가
+   */
   @GetMapping("/un-usable")
   public ResponseEntity<List<CouponResponse>> viewAllCanNotUseCoupons(@RequestParam String userId) {
     return ResponseEntity.ok(couponService.viewAllCanNotUseCoupons(userId));
