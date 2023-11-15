1f5156202111388b1271da25b9976613a74f986c
HanSeul <w0w1278@naver.com>
2023-11-15 21:44:56 +0900

    refactor: admin계정 3개 insert
    - admin 계정암호화 이슈로 인한 main 함수에 암호화 출력 삭제
    - 초기 비밀번호는 모두 'adminPassword'로 동일 추후 비밀번호 변경 후 이용 or 그냥 이용

--- src/main/resources/admin_data.sql
+++ src/main/resources/admin_data.sql
@@ -1,2 +1,10 @@
 INSERT INTO Admin (create_date_time, update_date_time, login_id, password, authority, last_login_date_time)
-VALUES (NOW(), NOW(), 'admin@naver.com', '$2a$10$zbSM.zjevI579kuDXlTNse6dJO4TMI6ejcmMYzkmHKhEwJWfVny8S', 'ROLE_ADMIN', NOW());
+VALUES (NOW(), NOW(), 'admin1@naver.com', '$2a$10$zbSM.zjevI579kuDXlTNse6dJO4TMI6ejcmMYzkmHKhEwJWfVny8S', 'ROLE_ADMIN', NOW());
+
+INSERT INTO Admin (create_date_time, update_date_time, login_id, password, authority, last_login_date_time)
+VALUES (NOW(), NOW(), 'admin2@naver.com', '$2a$10$zbSM.zjevI579kuDXlTNse6dJO4TMI6ejcmMYzkmHKhEwJWfVny8S', 'ROLE_ADMIN', NOW());
+
+INSERT INTO Admin (create_date_time, update_date_time, login_id, password, authority, last_login_date_time)
+VALUES (NOW(), NOW(), 'admin3@naver.com', '$2a$10$zbSM.zjevI579kuDXlTNse6dJO4TMI6ejcmMYzkmHKhEwJWfVny8S', 'ROLE_ADMIN', NOW());
+
+# password 는 모두 adminPassword로 동일
