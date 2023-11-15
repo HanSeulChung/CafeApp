5b5f29ead0ddcd85fc1f53779f27db6381ac3047
HanSeul <w0w1278@naver.com>
2023-11-15 17:41:42 +0900

    refactor: 401, 403에러 CustomException처럼 ErrorResponse build후 response에 보냄

--- src/main/java/com/chs/cafeapp/security/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/chs/cafeapp/security/JwtAuthenticationEntryPoint.java
@@ -1,5 +1,9 @@
 package com.chs.cafeapp.security;
 
+import static com.chs.cafeapp.exception.type.ErrorCode.UN_AUTHORIZATION;
+
+import com.chs.cafeapp.exception.dto.ErrorResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -9,11 +13,24 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+  private final ObjectMapper objectMapper = new ObjectMapper();
 
   @Override
   public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
+    ErrorResponse errorResponse;
+
     // 유효한 자격증명을 제공하지 않고 접근하려 할때 401
     response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-    response.getWriter().write("Unauthorized access");
+
+    errorResponse = ErrorResponse.builder()
+        .httpCode(401)
+        .errorCode(UN_AUTHORIZATION)
+        .ErrorMessage(UN_AUTHORIZATION.getDescription())
+        .build();
+
+    response.setContentType("application/json;charset=UTF-8");
+    response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
+    response.flushBuffer();
+
   }
 }
