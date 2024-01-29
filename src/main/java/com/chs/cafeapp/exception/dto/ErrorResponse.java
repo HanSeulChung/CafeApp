11e477d8744078e433018fd75b4e4a44aa6fe6eb
HanSeul <w0w1278@naver.com>
2024-01-29 16:46:06 +0900

    Style: ErrorResponse에 빌더 어노테이션 추가

--- src/main/java/com/chs/cafeapp/exception/dto/ErrorResponse.java
+++ src/main/java/com/chs/cafeapp/exception/dto/ErrorResponse.java
@@ -4,11 +4,9 @@ import com.chs.cafeapp.exception.type.ErrorCode;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
 @Builder
+@Getter
 @AllArgsConstructor
 public class ErrorResponse {
   private ErrorCode errorCode;
