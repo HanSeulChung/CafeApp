7bda8f7cc4e89173ca756283cb8b863261092dad
HanSeul <w0w1278@naver.com>
2024-02-28 16:14:00 +0900

    Fix: AdminRepository existsByLoginId 누락 수정

--- src/main/java/com/chs/cafeapp/auth/admin/repository/AdminRepository.java
+++ src/main/java/com/chs/cafeapp/auth/admin/repository/AdminRepository.java
@@ -8,4 +8,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface AdminRepository extends JpaRepository<Admin, Long> {
   Optional<Admin> findByLoginId(String loginId);
+  boolean existsByLoginId(String loginId);
 }
