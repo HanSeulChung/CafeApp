6757e68549269a5236a232e455fc2c90bb940ece
HanSeul <w0w1278@naver.com>
2024-02-28 16:17:12 +0900

    Refactor: 디렉토리 변경으로 삭제

--- src/main/java/com/chs/cafeapp/order/repository/OrderedMenuRepository.java
+++
@@ -1,8 +0,0 @@
-package com.chs.cafeapp.order.repository;
-
-import com.chs.cafeapp.order.entity.OrderedMenu;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderedMenuRepository extends JpaRepository<OrderedMenu, Long> {
-
-}
